b6479234fc4ff0fcd32dda76cd8c02702367e472
Charaaa11 <123252873+Charaaa11@users.noreply.github.com>
2023-07-10 23:42:55 +0400

    Update LoadingSpinner.jsx

--- src/component/blocks/LoadingSpinner.jsx
+++ src/component/blocks/LoadingSpinner.jsx
@@ -1,20 +1,20 @@
-import React from "react";
-import PulseLoader from "react-spinners/PulseLoader";
+// import React from "react";
+// import PulseLoader from "react-spinners/PulseLoader";
 
-function LoadingSpinner({ color }) {
-  return (
-    <div className="LoadingComponent">
-      <div className="LoaderAPP">
-        <PulseLoader
-          color={color}
-          loading={true}
-          size={20}
-          aria-label="Loading Spinner"
-          data-testid="loader"
-        />
-      </div>
-    </div>
-  );
-}
+// function LoadingSpinner({ color }) {
+//   return (
+//     <div className="LoadingComponent">
+//       <div className="LoaderAPP">
+//         <PulseLoader
+//           color={color}
+//           loading={true}
+//           size={20}
+//           aria-label="Loading Spinner"
+//           data-testid="loader"
+//         />
+//       </div>
+//     </div>
+//   );
+// }
 
-export default LoadingSpinner;
+// export default LoadingSpinner;
